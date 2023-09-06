8a9300520ccda88aa6434e578c14e3b085ee2349
Kyle Knight <knightkyle@outlook.com>
2023-09-05 23:49:52 -0700

    Lab 42

--- app/src/main/java/com/kyleknight/addtask/MainActivity.java
+++ app/src/main/java/com/kyleknight/addtask/MainActivity.java
@@ -1,16 +1,80 @@
 package com.kyleknight.addtask;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+import com.google.android.gms.ads.LoadAdError;
+import androidx.annotation.NonNull;
 
 public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
+    private RewardedAd mRewardedAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        MobileAds.initialize(this, initializationStatus -> {});
+
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        //  interstitial ad
+        InterstitialAd.load(this, "ca-app-pub-2296421973889521/2492917574", new AdRequest.Builder().build(),
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        mInterstitialAd = interstitialAd;
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        mInterstitialAd = null;
+                    }
+                });
+
+
+        Button showInterstitialButton = findViewById(R.id.showInterstitialButton);
+        showInterstitialButton.setOnClickListener(v -> {
+            if (mInterstitialAd != null) {
+                mInterstitialAd.show(MainActivity.this);
+            }
+        });
+
+        // Load a rewarded ad
+        RewardedAd.load(this, "ca-app-pub-2296421973889521/9971156181", new AdRequest.Builder().build(),
+                new RewardedAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+                        mRewardedAd = rewardedAd;
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        mRewardedAd = null;
+                    }
+                });
+
+
+        Button showRewardedButton = findViewById(R.id.showRewardedButton);
+        showRewardedButton.setOnClickListener(v -> {
+            if (mRewardedAd != null) {
+                mRewardedAd.show(MainActivity.this, rewardItem -> {
+
+                });
+            }
+        });
     }
 }
-
-test
